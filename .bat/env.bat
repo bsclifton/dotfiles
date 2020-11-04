9ecebb63d8a872408d3f830b22ef94649b334a25
Brian Clifton <brian@clifton.me>
2020-11-04 00:36:35 -0700

    Part 1 of trying to get clang-format working properly in SublimeText w/ multiple projects/workspaces

--- .bat/env.bat
+++ .bat/env.bat
@@ -1,6 +1,2 @@
-rem commenting out because this breaks other builds
-rem was needed though for clang_format to work in sublime :/
-rem setx CHROMIUM_BUILDTOOLS_PATH "C:\bb1\src\buildtools"
-
 rem requires admin command prompt to run
 setx /M PATH "%PATH%;%USERPROFILE%\.cargo\bin"
